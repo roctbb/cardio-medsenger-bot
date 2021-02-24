6b0ae991b5e29729b4a98d4e0b30be7f2425cae7
roctbb <roctbb@gmail.com>
2021-02-24 15:37:02 +0300

    add separate schedule

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-sudo mv agents_cardio.conf /etc/supervisor/conf.d/
-sudo mv agents_cardio_nginx.conf /etc/nginx/sites-available/
+sudo cp agents_cardio.conf /etc/supervisor/conf.d/
+sudo cp agents_cardio_nginx.conf /etc/nginx/sites-available/
 sudo supervisorctl update
 sudo systemctl restart nginx
 sudo certbot --nginx -d cardio.new.medsenger.ru
